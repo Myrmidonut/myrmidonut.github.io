4cd6497ae1d88f935b2ccc24fa3451055ee8ef13
Frederik <frederik-funke@gmx.de>
2018-04-07 23:40:04 +0200

    random quote machine

---
+++ random_quote_machine/script.js
@@ -0,0 +1,9 @@
+var apiUrl = "http://api.forismatic.com/api/1.0/";
+var request = new XMLHttpRequest();
+request.open("GET", apiUrl);
+request.responseType = "json";
+request.send();
+request.onload = function() {
+  var response = request.response;
+  console.log(response);
+}
