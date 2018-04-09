20ac2fe0b8c8db2d71539da37e5f5ca3888aa213
Frederik <frederik-funke@gmx.de>
2018-04-09 19:00:49 +0200

    local weather added

---
+++ local_weather/script.js
@@ -0,0 +1,95 @@
+/*
+current city
+temperature
+sky
+windspeed
+small image for weather, 50x50
+different background for current temperature
+button fahrenheit - celsius
+*/
+
+var url;
+var latitude;
+var longitude;
+var celsius = true;
+var temperature;
+var fahrenheit;
+
+var urlSummer = "https://images.pexels.com/photos/457881/pexels-photo-457881.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260";
+var urlSpring = "https://images.pexels.com/photos/46164/field-of-rapeseeds-oilseed-rape-blutenmeer-yellow-46164.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260";
+var urlAutumn = "https://images.pexels.com/photos/34001/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=750&w=1260";
+var urlWinter = "https://images.pexels.com/photos/325139/pexels-photo-325139.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+
+// Latitude: 47.0804206
+// Longitude: 15.4354491
+
+$(document).ready(function(){
+  getPosition();
+});
+
+function createUrl() {
+  url = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
+  console.log(url);
+}
+
+function getData() {
+  $.getJSON(url, function(e) {
+    $("#location").text(e.name);
+   // $("#temperature").text(e.main.temp + " C");
+    temperature = e.main.temp;
+    $("#temperature").text(temperature + " C");
+    $("#sky").text(e.weather[0].description);
+    $("#windspeed").text(e.wind.speed + " knots");
+    $("#image").html("<img src=" + e.weather[0].icon + '">');
+    
+  //  var temp = e.main.temp;
+    if (temperature >= 20) {
+      $("main").css("background-image", "url(" + urlSummer + ")");
+    } else if (temperature >= 10 && temperature < 20) {
+      $("main").css("background-image", "url(" + urlSpring + ")");
+    } else if (temperature >= 0 && temperature < 10) {
+      $("main").css("background-image", "url(" + urlAutumn + ")");
+    } else {
+      $("main").css("background-image", "url(" + urlWinter + ")");
+    }
+    
+  });
+}
+
+function getPosition() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(e) {
+      console.log(`Latitude : ${e.coords.latitude}`);
+      console.log(`Longitude: ${e.coords.longitude}`);
+      latitude = e.coords.latitude;
+      longitude = e.coords.longitude;
+      
+      createUrl();
+      getData()
+      
+    });
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+$("#temperature").on("click", function() {
+ // if (Boolen(temperature) == true) {
+    if (celsius == true) {
+      fahrenheit = Math.floor((temperature * 1.8 + 32)*100) / 100;
+      $("#temperature").text(fahrenheit + " F");
+      celsius = false;
+    } else {
+      $("#temperature").text(temperature + " C");
+      celsius = true;
+    }
+//  }
+})
+
+function debug() {
+  //celsius = true;
+  latitude = 47;
+  longitude = 15;
+  createUrl();
+  getData();
+}
--- random_quote_machine/script.js
+++ random_quote_machine/script.js
@@ -1,9 +1,16 @@
+
 var apiUrl = "https://api.forismatic.com/api/1.0/";
 var request = new XMLHttpRequest();
 request.open("GET", apiUrl);
-request.responseType = "json";
+request.responseType = "jsonp";
 request.send();
 request.onload = function() {
   var response = request.response;
   console.log(response);
 }
+
+
+//var test = JSON.parse("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=JSON&lang=en");
+//var test = JSON.parse("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback");
+
+//console.log(test);
